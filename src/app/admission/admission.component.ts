649660fa6abd214e9881b21b86569c42c16b082f
Muhammad-Amer1997 <amer.rasool121@gmail.com>
2019-05-17 08:49:18 +0500

    BedDailogFlowAdded(TaskDone)

--- src/app/admission/admission.component.ts
+++ src/app/admission/admission.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 import { SelectItem, MessageService } from "primeng/api";
 import { Admission } from "./admission";
+import { Location } from "@angular/common";
 
 @Component({
   selector: "app-admission",
@@ -11,24 +12,26 @@ import { Admission } from "./admission";
 })
 export class AdmissionComponent implements OnInit {
   bedType: SelectItem[];
-  ProgressSpinnerDlg :boolean
+  ProgressSpinnerDlg: boolean;
   // SelectedBed: string;
 
   public _admission = new Admission(); //instance of the model file
 
-  constructor(private router: Router, private admissionSer: AdmissionService, private messageService: MessageService) {}
+  constructor(
+    private router: Router,
+    private admissionSer: AdmissionService,
+    private messageService: MessageService,
+    private location: Location
+  ) {}
 
   ngOnInit() {
     this.bedsDropdown();
     this.ProgressSpinnerDlg = true;
   }
 
-
-
-  backToMainScreen() {
-    this.router.navigate(["/mainscreen"]);
+  back() {
+    this.location.back();
   }
-
 
   bedsDropdown() {
     this.bedType = [
@@ -37,35 +40,30 @@ export class AdmissionComponent implements OnInit {
     ];
   }
 
-
   submitAdmission() {
     console.log(this._admission);
     this.admissionSer.saveAdmission(this._admission).subscribe(
       data => {
-      console.log(data);
-      this.messageService.add({
-        severity: "success",
-        summary: "Succesfully",
-        detail : "Beds added successsfully"
-      });
-    },
-    error => {
-      this.messageService.add({
-        severity: "error",
-        summary: "Error occured,Please Reload Your Page"
-      });
-      console.log(error);
-    }
+        console.log(data);
+        this.messageService.add({
+          severity: "success",
+          summary: "Succesfully",
+          detail: "Beds added successsfully"
+        });
+      },
+      error => {
+        this.messageService.add({
+          severity: "error",
+          summary: "Error occured,Please Reload Your Page"
+        });
+        console.log(error);
+      }
     );
-    this.admissionSer.getAvailableBeds().subscribe(data=>{
-// tslint:disable-next-line: forin
-            for(const i in data){
-              console.log(data[i]);
-            }
-    })
+    this.admissionSer.getAvailableBeds().subscribe(data => {
+      // tslint:disable-next-line: forin
+      for (const i in data) {
+        console.log(data[i]);
+      }
+    });
   }
-
-
-
-
 }
