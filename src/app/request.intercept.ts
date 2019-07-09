5c082d4f49e73d7e0037ba576041ea0d69d047a6
shezad <shahzadiqbal.may18@gmail.com>
2019-07-09 23:07:03 +0500

    done jwt

--- src/app/request.intercept.ts
+++ src/app/request.intercept.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
 export class NoopInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
       console.log("hello interceptor");
-    const changedReq = req.clone({headers: req.headers.set('Authorization', 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJzQHMuY29tIiwic2NvcGVzIjpbeyJhdXRob3JpdHkiOiJST0xFX0FETUlOIn1dLCJpc3MiOiJodHRwOi8vZGV2Z2xhbi5jb20iLCJpYXQiOjE1NjI2MTE2ODAsImV4cCI6MTU2MjYyOTY4MH0.ByjRSoucLOgqoYbIHGYZudKQnYqfpFqze6AZdI0HQxU')});
+    const changedReq = req.clone({headers: req.headers.set('Authorization', sessionStorage.getItem('token'))});
     return next.handle(changedReq);
   }
 }
