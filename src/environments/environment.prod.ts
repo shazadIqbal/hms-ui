098f51e7852a715aef4431e3af7f49f78ce6481f
Muhammad-Amer1997 <amer.rasool121@gmail.com>
2019-10-03 23:29:41 +0500

    enviroment updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   tokenURL: 'http://localhost:8080/',
-  baseUrl: 'http://localhost:8081/',
+  baseUrl: 'http://localhost:8080/',
   opdUrl: 'http://localhost:8080/',
-  labUrl: 'http://localhost:8082/',
-  pharmacyUrl: 'http://localhost:8081/'
+  labUrl: 'http://localhost:8081/',
+  pharmacyUrl: 'http://localhost:8082/'
 };
